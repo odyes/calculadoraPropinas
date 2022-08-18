13949a262d954d67e465d0a12ddcb1c9150c47a6
Helliud <javiereliudliz@gmail.com>
2022-08-18 09:55:16 -0700

    calculadora

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalConPropina: UILabel!
+    @IBOutlet weak var totalPropina: UILabel!
+    @IBOutlet weak var porcentaje: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var cuenta: UITextField!
+   
+    
+    @IBAction func doChangePropina(_ sender: Any) {
+        lblProcentajePropina.text = "\(Init(slider.value)%"
+        let cuenta = Float(txtCuenta.text!) ?? 0.0
+        
+        let pocentajePropina = Float(Init(slider.value))
+        let propina = cuenta * porcentajePropina / 100.0
+        
+        totalPropina.text = "$\(propina)"
+        //lblMontoPropina.text = String(format: "$ %.2f", propina)
+        
+        
+        let total = propina + cuenta
+        totalConPropina.text = "$\(total)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
