1cb7d38a2910bcc36c73cf68ae9957e61347182f
Helliud <javiereliudliz@gmail.com>
2022-08-23 09:19:12 -0700

    hola

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -1,47 +1,48 @@
 //
 //  ViewController.swift
-//  calculadora
+//  calculadora-propinas
 //
 //  Created by Alumno on 8/18/22.
-//  Copyright © 2022 ulsa. All rights reserved.
+//  Copyright © 2022 ULSA. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var totalConPropina: UILabel!
-    @IBOutlet weak var totalPropina: UILabel!
-    @IBOutlet weak var porcentaje: UILabel!
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var cuenta: UITextField!
-   
     
-    @IBAction func doChangePropina(_ sender: Any) {
-        lblProcentajePropina.text = "\(Init(slider.value)%"
-        let cuenta = Float(txtCuenta.text!) ?? 0.0
-        
-        let pocentajePropina = Float(Init(slider.value))
-        let propina = cuenta * porcentajePropina / 100.0
-        
-        totalPropina.text = "$\(propina)"
-        //lblMontoPropina.text = String(format: "$ %.2f", propina)
-        
-        
-        let total = propina + cuenta
-        totalConPropina.text = "$\(total)"
-    }
+    var porcentaje :Float = 0
+    
+    
+    
+    @IBOutlet weak var tfldTotal: UITextField!
+    @IBOutlet weak var lblPorcentaje: UILabel!
+    @IBOutlet weak var sldPropina: UISlider!
+    @IBOutlet weak var lblPropina: UILabel!
+    @IBOutlet weak var lblTotal: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func doChangeValue(_ sender: Any) {
+        porcentaje = sldPropina.value
+        lblPorcentaje.text = "\(round(porcentaje))%"
+        
+        let totalStr = tfldTotal.text!
+        
+        let total = Float(totalStr) ?? 0.0
+        
+        let resultadoPropina = (total * (porcentaje/100.0))
+        
+        let cantidadTotal = total + resultadoPropina
+        
+        lblPropina.text = String(format: "$ %.2f",resultadoPropina)
+        
+        lblTotal.text = String(format: "$ %.2f",cantidadTotal)
+        
+        
     }
 }
-
-
